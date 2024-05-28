54f881a7e11b9bc478708818555f4adccb8c910c
dekolor <stefanbriceag1@gmail.com>
2024-05-28 12:29:12 +0300

    initial commit

---
+++ src/main/java/ro/dekolor/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package ro.dekolor.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
